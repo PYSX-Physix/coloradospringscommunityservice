47ba69aeb654220c3baeff01f4dc7d43093435bb
Physix <lolathecat16@outlook.com>
2025-11-12 00:19:06 -0700

    I'm going to lose my mind

--- src/YourPosts.tsx
+++ src/YourPosts.tsx
@@ -19,8 +19,15 @@ function YourPosts() {
 
     if (res.ok) {
       console.log("Post submitted!");
+      fetchPosts();
     }
   };
+
+  const fetchPosts = async () => {
+    const res = await fetch("/api/posts");
+    const data = await res.json();
+  };
+
 
 
   return (
