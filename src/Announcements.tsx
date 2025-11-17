ea94b40ccab3cc14f04eb1a232a87a59447d6e5f
Physix <lolathecat16@outlook.com>
2025-11-17 10:49:36 -0700

    added announcements page and first announcement

--- src/Announcements.tsx
+++ src/Announcements.tsx
@@ -1,4 +1,5 @@
-import { Title1 } from "@fluentui/react-components";
+import { Card, CardHeader, Text, Title1, Title3 } from "@fluentui/react-components";
+import { WrenchScrewdriver24Color } from "@fluentui/react-icons";
 import React from "react";
 
 export default function Announcements()
@@ -6,7 +7,16 @@ export default function Announcements()
     return(
         <div>
             <Title1>Announcements</Title1>
-            
+            <Card>
+                <CardHeader header={<Title3><WrenchScrewdriver24Color /> Minor Update: V11.17.2025.03</Title3>}/>
+                <p>
+                    <Text>
+                        <strong>Fix:</strong> Height of the page doesn't adjust to the size of the display.<br/>
+                        <strong>Addition:</strong> Announcements page. This will be used to show update patch notes and disclosures.<br/>
+                        <strong>Temp Additon:</strong> Warning for the application being in development.
+                    </Text>
+                </p>
+            </Card>
         </div>
     );
 }
