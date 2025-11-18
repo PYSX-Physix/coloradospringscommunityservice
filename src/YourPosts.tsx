6403467de2597a057935dc85c4e7853c0d51308d
Physix <lolathecat16@outlook.com>
2025-11-17 17:54:13 -0700

    resolved merge conflicts

--- src/YourPosts.tsx
+++ src/YourPosts.tsx
@@ -124,11 +124,7 @@ function YourPosts() {
           </DialogSurface>
         </Dialog>
       </div>
-<<<<<<< HEAD
       <Table style={{marginTop: '15px'}} aria-label="Your Posts Table" id="yourpoststable" sortable>
-=======
-      <Table style={{marginTop: '16px'}} aria-label="Your Posts Table" id="yourpoststable" sortable>
->>>>>>> 9c2b0b87ad1692ac39060614a9a86e41bb5168f9
         <TableHeader>
           <TableRow>
             {columns.map((column) => (
