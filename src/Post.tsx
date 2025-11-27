c8f588c09c67fb650532f58c4ea27843b083f286
Physix <lolathecat16@outlook.com>
2025-11-26 19:02:10 -0600

    Fixed the buttons not being layed out properly

--- src/Post.tsx
+++ src/Post.tsx
@@ -136,7 +136,7 @@ export default function Post() {
 
   return (
     <div className="scrollbox">
-      <div style={{width: '100vh'}}>
+      <div style={{width: '100vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
         <Title1>{post.title}</Title1>
         <div style={{ display: 'flex', gap: '8px', marginTop: '16px' }}>
           {!isOrganizer && (
