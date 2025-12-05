1a4cfc40ffce708012ea906ef50b30766500a5ae
Physix <lolathecat16@outlook.com>
2025-12-05 13:38:47 -0700

    fixed formatting for Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -169,16 +169,16 @@ function Home() {
             )}
             <div className="content-scroll">
                 <Routes>
-                <Route path="/" element={<Posts />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/saved/my-posts" element={<YourPosts />} />
-                <Route path="/about" element={<About/>}/>
-                <Route path='/post' element={<Post/>}/>
-                <Route path='/announcements' element={<Announcements/>}/>
-                <Route path='/policies/privacy-policy' element={<Privacy/>}/>
-                <Route path='/policies/terms-of-service' element={<Terms/>}/>
-                <Route path='/profile' element={<Profile/>}/>
-                <Route path='/help' element={<HelpHome/>}/>
+                    <Route path="/" element={<Posts />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/saved/my-posts" element={<YourPosts />} />
+                    <Route path="/about" element={<About/>}/>
+                    <Route path='/post' element={<Post/>}/>
+                    <Route path='/announcements' element={<Announcements/>}/>
+                    <Route path='/policies/privacy-policy' element={<Privacy/>}/>
+                    <Route path='/policies/terms-of-service' element={<Terms/>}/>
+                    <Route path='/profile' element={<Profile/>}/>
+                    <Route path='/help' element={<HelpHome/>}/>
                 </Routes>
             </div>
         </div>
