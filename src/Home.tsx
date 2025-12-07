65b0e8b3e00f578ba52b6434d257b2123e429386
Physix <lolathecat16@outlook.com>
2025-12-07 16:19:48 -0700

    Add NavDrawerFooter with Beta tag to Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -28,7 +28,9 @@ import {
   NavSubItemGroup,
   MenuDivider,
   Tooltip,
-  Hamburger
+  Hamburger,
+  NavDrawerFooter,
+  Tag
 } from "@fluentui/react-components";
 
 import {
@@ -174,6 +176,9 @@ function Home() {
             </NavCategory>
             <NavItem as='a' href='/help' value={HELPHOME} icon={<QuestionCircle20Color/>}>Help</NavItem>
           </NavDrawerBody>
+          <NavDrawerFooter>
+            <Tag shape='circular' appearance='brand'>Beta</Tag>
+          </NavDrawerFooter>
         </NavDrawer>
 
         <div className="content">
